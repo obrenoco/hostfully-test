7a4b5a4c2ad5bf7552ac04cbdab9c29f8cff5603
Breno Romeiro <breno.romeiro@Brenos-MacBook-Pro.local>
2023-12-04 01:55:04 -0300

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import "antd/dist/antd.min.css";
 import { BookingsProvider } from "./pages/Bookings/context";
-import { Bookings } from "./pages/Bookings";
+import { Bookings } from "./pages/Bookings/Index";
 import { Header } from "./components/Header";
 
 function App() {
